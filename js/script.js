86ce209fb8f848b1a7fe8cdd6f7950230f93b43b
ThallesSorrilha <thallessorrilha@gmail.com>
2025-04-28 15:22:21 -0300

    feat(portfolio): dark/light mode memory

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const localTheme = localStorage.getItem("theme");
+  if (localTheme != null) {
+    updateBody(localTheme);
+    updateIcon(localTheme);
+  }
+});
+
 function changeTheme() {
   const theme = document.body.getAttribute("data-theme");
   const newTheme = theme == "dark" ? "light" : "dark";
 
   localStorage.setItem("theme", newTheme);
-  document.body.setAttribute("data-theme", newTheme);
 
+  updateBody(newTheme);
+  updateIcon(newTheme);
+}
+
+function updateBody(theme) {
+  document.body.setAttribute("data-theme", theme);
+}
+
+function updateIcon(theme) {
   const iTheme = document.getElementById("icon-dark-light");
   iTheme.classList =
-    newTheme == "dark"
+    theme == "dark"
       ? "bi bi-sun-fill icon-dark-light"
       : "bi bi-moon-fill icon-dark-light";
 }
